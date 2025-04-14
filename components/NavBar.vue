610c3218423307f68d8c9c4557fa6d20de18eb0b
PhantomSquire <90224153+PhantomSquire@users.noreply.github.com>
2025-04-13 20:42:46 -0700

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,17 +4,21 @@
   
     <span>HOME</span>
     
-    <br>
-    
-    <span>HOME</span>
-    
-    <br>
-    
-    <span>HOME</span>
-    
-    <br>
-    
-    <span>HOME</span>
+    <span>ROLES</span>
+
+    <span>ABOUT</span>
+
+    <span>GUIDE</span>
+
+    <span>FAQ</span>
+
+    <span>UPDATES</span>
+
+    <span>RESOURCES</span>
+
+    <span>COMMUNITY</span>
+
+    <span>CONTRIBUTE!</span>
   
   </navbar>
   
