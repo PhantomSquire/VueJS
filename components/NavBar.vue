40b1abb1fcbdd811379baf75a22031203adbe86c
PhantomSquire <90224153+PhantomSquire@users.noreply.github.com>
2025-04-13 20:58:36 -0700

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar">
+  <nav>
     <a href="/">HOME</a>
     <a href="/">ROLES</a>
     <a href="/">ABOUT</a>
@@ -11,26 +11,3 @@
     <a href="/">CONTRIBUTE!</a>
   </nav>
 </template>
-
-<style scoped>
-.navbar {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  background-color: #1e1e1e;
-  padding: 1rem;
-  justify-content: center;
-  border-radius: 10px;
-}
-
-.navbar a {
-  color: white;
-  text-decoration: none;
-  font-weight: bold;
-  transition: color 0.3s ease;
-}
-
-.navbar a:hover {
-  color: #00bfff;
-}
-</style>
