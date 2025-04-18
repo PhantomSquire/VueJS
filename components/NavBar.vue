8fe10e1a00d58b3ea16c2ef55c6e3141672a4f1f
PhantomSquire <90224153+PhantomSquire@users.noreply.github.com>
2025-04-18 13:24:59 -0700

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,5 @@
 <template>
-  <nav>
-    <a href="/">HOME</a>
-    <a href="/">ROLES</a>
-    <a href="/">ABOUT</a>
-    <a href="/">GUIDE</a>
-    <a href="/">FAQ</a>
-    <a href="/">UPDATES</a>
-    <a href="/">RESOURCES</a>
-    <a href="/">COMMUNITY</a>
-    <a href="/">CONTRIBUTE!</a>
-  </nav>
+  
+  <a href="/">HOME</a>
+  
 </template>
